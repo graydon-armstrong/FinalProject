fae7fabedcf74065d7ed8abf34e6c42b4de4ede4
Graydon Armstrong <seeds77222@hotmail.com>
2013-07-30 01:03:12 -0400

    Basic Game loop

--- projectV1.py
+++ projectV1.py
@@ -5,3 +5,27 @@
 # Date Last Modified: July 25th, 2013
 # Program description: A Final Project game
 # Revision History: Version 1 is setting up a basic game framework
+
+import pygame, random
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+       
+def main():   
+    keepGoing = True    
+    clock = pygame.time.Clock()
+    while keepGoing:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+            elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            keepGoing = False 
+#run the main menthod at the start
+if __name__ == "__main__":
+    main()
